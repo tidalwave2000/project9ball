21ad506499e02ded373a4ba7578587e1e117f8c6
Pierce Puckette <ppuckette@gmail.com>
2022-01-01 04:00:23 -0500

    javascript

---
+++ js/scripts9ball.js
@@ -0,0 +1,36 @@
+function playerInfo() {
+    const teamNumber1 = document.getElementById("teamNum");
+    const teamNumber2 = document.getElementById("teamNum2");
+    const playerNumber1 = document.getElementById("playerNum1");
+    const playerNumber2 = document.getElementById("playerNum2");
+    const playerNam1 = document.getElementById("playerName1");
+    const playerNam2 = document.getElementById("playerName2");
+    const playerRank1 = document.getElementById("playerSkill1");
+    const playerRank2 = document.getElementById("playerSkill2");
+
+    alert("Player Information Saved");
+    window.location.href = "lag9-ball.html";
+}
+
+function lag() {
+    const wonLag = document.getElementById("playerNum");
+    alert("Player Information Saved");
+    window.location.href = "scoreSheet9-ball.html";
+}
+
+function scoreSheet() {
+    
+}
+
+function matchPoints() {
+    
+}
+
+function totalTeamScore() {
+    
+}
+
+function endOfMatches() {
+
+}
+    
